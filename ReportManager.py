0d1828569a40ced714c1ec61ab51a429c65c0a3f
KeiShimon <simonisimonisi@gmail.com>
2019-04-24 11:17:33 +0900

    Now ReportManager can be used with cli.

--- ReportManager.py
+++ ReportManager.py
@@ -1,89 +1,96 @@
 #%%
 import numpy as np
 import pandas as pd
+import sys
 from pandas import Series, DataFrame
 
 #%%
-filepass, INITIALPOT = '3B.P SB vs BTN PairBoards.csv', 200
-df = pd.read_csv(filepass,header=3)
-df.head()
+def main(*argv):
 
-#%%
-
-changeColName = {'Global %':'Occurence'}
-for colname in df.columns:
-
-    if 'EV' in colname:
-        del df[colname]
-
-    elif 'BET' in colname:
-        i = colname.find('%')
-        betsize = str(int(colname[4:i]) * 100 / INITIALPOT)
-        changeColName[colname] = 'Bet' + betsize
-
-    elif 'CHECK' in colname:
-        changeColName[colname] = 'Check'
+    PATH = argv[0]
+    try:
+        INITIALPOT = int(argv[1])
+    except:
+        INITIALPOT = 100
     
-    elif 'Global' not in colname:
-        changeColName[colname] = colname.replace(' ', '_').replace('EQR','EqR').replace('Equity','Eq')
+    df = pd.read_csv(PATH,header=3)
 
-df = df.rename(columns=changeColName)
+    # change column name
+    changeColName = {'Global %':'Occurence'}
 
+    for colname in df.columns:
 
-flops = Series(df['Flop'])
-rankToInt = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'T':10,'J':11,'Q':12,'K':13,'A':14}
-top,mid,btm = [], [], []
-tone,pair = [], []
+        if 'EV' in colname:
+            del df[colname]
 
-for flop in flops:
+        elif 'BET' in colname or 'RAISE' in colname:
+            i = colname.find('%')
+            betsize = str(int(colname[4:i]) * 100 / INITIALPOT)
+            action = 'Bet' if 'BET' in colname else 'Raise'
+            changeColName[colname] = action + betsize
 
-    if len(flop) > 3:
+        elif 'CHECK' in colname:
+            changeColName[colname] = 'Check'
+        
+        elif 'Global' not in colname:
+            changeColName[colname] = colname.replace(' ', '_').replace('EQR','EqR').replace('Equity','Eq')
 
-        # rank and pair
-        t,m,b = rankToInt[flop[0]], rankToInt[flop[3]], rankToInt[flop[6]]
-        top.append(t)
-        mid.append(m)
-        btm.append(b)
+    df = df.rename(columns=changeColName)
 
-        if t != b:
-            if t!= m and m != b:
-                pair.append(0)
-            else:
-                pair.append(10*m)
+    # insert columns that describe flop profile
+    flops = Series(df['Flop'])
+    rankToInt = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'T':10,'J':11,'Q':12,'K':13,'A':14}
+    top,mid,btm = [], [], []
+    tone,pair = [], []
 
-        else:
-            pair.append(100*t)
+    for flop in flops:
 
-        # tone
-        t, m, b = flop[1::3]
+        if len(flop) > 3:
 
-        if t == m:
+            # rank and pair
+            t,m,b = rankToInt[flop[0]], rankToInt[flop[3]], rankToInt[flop[6]]
+            top.append(t)
+            mid.append(m)
+            btm.append(b)
+
             if t != b:
+                if t!= m and m != b:
+                    pair.append(0)
+                else:
+                    pair.append(10*m)
+            else:
+                pair.append(100*t)
+
+            # tone
+            t, m, b = flop[1::3]
+
+            if t == m:
+                if t != b:
+                    tone.append(2)
+                else:
+                    tone.append(1)
+            elif t == b or m == b:
                 tone.append(2)
             else:
-                tone.append(1)
-
-        elif t == b or m == b:
-            tone.append(2)
+                tone.append(3)
 
         else:
-            tone.append(3)
-
-    else:
-        top.append(0)
-        mid.append(0)
-        btm.append(0)
-        pair.append(0)
-        tone.append(0)
+            top.append(0)
+            mid.append(0)
+            btm.append(0)
+            pair.append(0)
+            tone.append(0)
 
 
-df.insert(2,'tone',tone)
-df.insert(2,'pair',pair)
-df.insert(2,'btm',btm)
-df.insert(2,'mid',mid)
-df.insert(2,'top',top)
+    df.insert(2,'tone',tone)
+    df.insert(2,'pair',pair)
+    df.insert(2,'btm',btm)
+    df.insert(2,'mid',mid)
+    df.insert(2,'top',top)
 
-df.to_csv(filepass,index=None)
-df.head()
+    df.to_csv(PATH,index=None)
 
-#%%
+    sys.exit()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
